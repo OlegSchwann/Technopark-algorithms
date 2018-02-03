858f0ed3e598e8c5d3b0fd0fdaf38242d84c2ea2
Oleg Schwann <cup.of.software.code@gmail.com>
2018-02-03 17:43:35 +0300

    Удалено лишнее, подправлен sql скрипт

--- database/scratch.sql
+++ database/scratch.sql
@@ -61,4 +61,27 @@ CREATE SUBSCRIPTION subscription_tables2 -- for slave2
   CONNECTION 'host=192.168.1.151 port=5404 user=postgres dbname=postgres'
   PUBLICATION publication_tables2;
 
-INSERT INTO game (id, name, developer, description) VALUES (1, 'Dota', 'valve', 'Самая задротская игра последнего десятилетия');
+-- для добавления тестовых данных.
+INSERT INTO gamer(id, team_id, nikname, full_name, description) VALUES
+  (1, 1, 'RodjER', 'Владимира Никогосяна', 'Недавно перешёл в команду'),
+  (2, 2, 'Lil', 'Ильи Ильюка', 'Недавно перешёл в команду');
+INSERT INTO team (id, name, description) VALUES
+  (1, 'Natus Vincere', 'Единственная команда о которой я слышал. Друг увлекался ставками на киберспорт.'),
+  (2, 'Virtus.pro', 'Довольно сильная команда. Оказывается, за игроками стоят крупные корпорации. Реклама во время трансляций.');
+INSERT INTO game (id, name, developer, description) VALUES
+  (1, 'Dota', 'valve', 'Самая задротская игра последнего десятилетия');
+INSERT INTO tourney(id, game_id, description) VALUES
+  (1, 1, 'Chf;tybt');
+INSERT INTO match (id, tourney_id, team_1_id, team_2_id, score_1_team, score_2_team, description, broadcast_link) VALUES
+  (1, 1, 1, 2, 5, 6, 'потная игра', 'https://www.youtube.com/');
+
+
+-- для извлечения дданных.
+SELECT id, team_id, nikname, full_name, description FROM gamer WHERE team_id = ?;
+SELECT id, name, description FROM team WHERE id = ?;
+INSERT INTO game (id, name, developer, description) VALUES
+  (1, 'Dota', 'valve', 'Самая задротская игра последнего десятилетия');
+INSERT INTO tourney(id, game_id, description) VALUES
+  (1, 1, 'Chf;tybt');
+INSERT INTO match (id, tourney_id, team_1_id, team_2_id, score_1_team, score_2_team, description, broadcast_link) VALUES
+  (1, 1, 1, 2, 5, 6, 'потная игра', 'https://www.youtube.com/');
